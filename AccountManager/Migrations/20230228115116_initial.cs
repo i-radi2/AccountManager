e99045a5eb9982630cb4dfa2ef56297d4ea27313
Ibrahim Radi <ibrahim.radi@nagwa.com>
2023-02-28 14:00:06 +0200

    Add Project files

---
+++ AccountManager/Migrations/20230228115116_initial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountManager.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "OnlineAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserId = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Notes = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OnlineAccounts", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OnlineAccounts");
+        }
+    }
+}
---
+++ AccountManager/Models/OnlineAccount.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AccountManager.Models;
+
+public class OnlineAccount
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Account Name cannot be empty")]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "User Name should be specified")]
+    [StringLength(50)]
+    public string UserId { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password must be specified")]
+    [StringLength(50)]
+    public string Password { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    [MaxLength(500)]
+    public string Notes { get; set; } = string.Empty;
+}
